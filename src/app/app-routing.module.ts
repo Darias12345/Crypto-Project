2de4e7a0dbcbc319caa21461353893c728bc1320
daria <dariasmaja17@gmail.com>
2023-09-24 21:18:17 +0200

    Code refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
 import { CryptoDetailComponent } from './crypto-detail/crypto-detail.component';
-import { AddCurrencyComponent } from './add-currency/add-currency.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/list', pathMatch: 'full' },
   { path: 'list', component: CryptoListComponent },
   { path: 'detail/:id', component: CryptoDetailComponent },
-  { path: 'add', component: AddCurrencyComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,15 +14,13 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { AddCurrencyComponent } from './add-currency/add-currency.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CryptoListComponent,
     CryptoDetailComponent,
-    AddCurrencyComponent,
-
   ],
   imports: [
     BrowserModule,
